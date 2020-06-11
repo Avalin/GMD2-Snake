869e3491fc9a935235124e74b8c6df24640e655b
Avalin <Avalin@MSI>
2020-06-11 14:04:52 +0200

    Snake Init

--- SnakeApplication/Snake.cs
+++ SnakeApplication/Snake.cs
@@ -8,13 +8,28 @@ namespace SnakeApplication
 {
     class Snake
     {
-        int score = 0;
-        int length = 3;
+        bool debug = true;
+        int score;
+        int length;
+
+        public Snake(int length) 
+        {
+            score = 0;
+            this.length = length;
+        }
 
         void EatFood(Food food) 
         {
             score += food.Value;
             length++;
+
+            #region Debug Tools
+            if (debug) 
+            {
+                Console.WriteLine("Snake ate " + food.Type + " for a value of " + food.Value + ".");
+                Console.WriteLine("Snake now has a score of " + score + " and a length of " + length + ".");
+            }
+            #endregion Debug Tools
         }
     }
 }
