337cddfc57522ee25a53835faf78a0a4f5516731
Avalin <Avalin@MSI>
2020-06-11 13:12:04 +0200

    Refactor

--- SnakeApplication/GameWindow.cs
+++ SnakeApplication/GameWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,9 +23,47 @@ namespace SnakeApplication
             Environment.Exit(0);
         }
 
-        private void GameLoop()
+        internal void GameLoop()
         {
-            
+            TimeSpan MS_PER_FRAME = TimeSpan.FromMilliseconds(1.0 / 60.0 * 10000.0);
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            TimeSpan previous = stopWatch.Elapsed;
+            TimeSpan lag = new TimeSpan(0);
+            while (true)
+            {
+
+                TimeSpan current = stopWatch.Elapsed;
+                TimeSpan elapsed = current - previous;
+                previous = current;
+                lag += elapsed;
+
+                //ProcessInput();
+                //Fixed timestep for logics, varying for rendering
+                while (lag >= MS_PER_FRAME)
+                {
+                    UpdateGameLogic();
+
+                    lag -= MS_PER_FRAME;
+                }
+                RenderToScreen();
+                Refresh();
+            }
+        }
+
+        private void ProcessInput(object sender, KeyEventArgs e)
+        {
+            //Input events here
+        }
+
+        private void RenderToScreen()
+        {
+            //Render events here
+            Application.DoEvents();
+        }
+
+        private void UpdateGameLogic()
+        {
+
         }
     }
 }
