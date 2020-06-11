e6a03aa7a0e5256d670b12fdb886b8a14c6457b6
Avalin <Avalin@MSI>
2020-06-11 13:58:54 +0200

    Snake Class Initialized

---
+++ SnakeApplication/Snake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeApplication
+{
+    class Snake
+    {
+        int score = 0;
+        int length = 3;
+
+        void EatFood(Food food) 
+        {
+            score += food.Value;
+            length++;
+        }
+    }
+}
